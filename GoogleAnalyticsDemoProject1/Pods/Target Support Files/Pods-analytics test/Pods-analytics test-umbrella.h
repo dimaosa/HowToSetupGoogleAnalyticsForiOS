6aaa0545a383ef4f0139d9985c648bfdb2980cbc
Dima Osadchy <dima.osadchy@starcut.com>
2017-04-07 14:44:02 +0300

    changed folder name

--- GoogleAnalyticsDemoProject1/Pods/Target Support Files/Pods-analytics test/Pods-analytics test-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_analytics_testVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_analytics_testVersionString[];
-
--- GoogleAnalyticsDemoProject1/analytics test-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-//#import <GoogleAnalytics.h>
-#import "GAI.h"
-#import "GAIDictionaryBuilder.h"
-#import "GAIFields.h"
-#import "GAILogger.h"
