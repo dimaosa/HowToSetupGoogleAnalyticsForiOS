4ae234090526891af1e78341c1c592f510eb8ca7
Dima Osadchy <dima.osadchy@starcut.com>
2017-04-07 14:51:47 +0300

    changes folder name

---
+++ GoogleAnalyticsDemoProject1/Pods/Target Support Files/Pods-analytics test/Pods-analytics test-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_analytics_testVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_analytics_testVersionString[];
+
